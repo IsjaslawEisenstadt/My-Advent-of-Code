8b0e2769d490fcbb5b7d4e583b4082d6f94f4c5b
Isjaslaw Eisenstadt <i.eisenstadt@gmx.de>
2022-12-02 15:27:10 +0100

    fix typo

--- src/day1.rs
+++ src/day1.rs
@@ -12,19 +12,19 @@ fn test_find_most_calories_for_one_elf() -> Result<()> {
 }
 
 #[test]
-fn test_find_most_calories_for_three_elfs() -> Result<()> {
+fn test_find_most_calories_for_three_elves() -> Result<()> {
 	assert_eq!(find_highest_calories(File::open("inputs/day1.txt")?, 3)?, 199172);
 	Ok(())
 }
 
-fn find_highest_calories(file: File, num_elfs: usize) -> Result<u32> {
-	if num_elfs == 0 {
-		bail!("Number of elf's must be greater than 0!");
+fn find_highest_calories(file: File, num_elves: usize) -> Result<u32> {
+	if num_elves == 0 {
+		bail!("Number of elves must be greater than 0!");
 	}
 
 	let reader = BufReader::new(file);
 	let mut current_elf_calories: u32 = 0;
-	let mut highest_calories: Vec<u32> = std::iter::repeat(0).take(num_elfs).collect();
+	let mut highest_calories: Vec<u32> = std::iter::repeat(0).take(num_elves).collect();
 
 	for line in reader.lines() {
 		if let Ok(parse_result) = line?.parse::<u32>() {
